79250e1c8cfac361b6946a5a4ed291e2b3f1ea1e
Mert Kalkancı <mertkalkanci05@hotmail.com>
2023-08-06 14:26:39 +0300

    update chat style & config

--- code/main.py
+++ code/main.py
@@ -15,6 +15,7 @@ IF User wants you to set a reminder respond with "I will set a reminder at x tim
 IF User wants you to arranga a meeting respond with "I will arrange a meeting at x time"
 IF User wants you to send an email respond with "I will send an email to x with content y"
 IF User wants you to send a text respond with "I will send a text to x with content y"
+IF User says something else respond normally
 USER:
 Arrange a meeting at 3pm
 BOT:
@@ -25,7 +26,7 @@ USER:
 class ChatterPage(ft.UserControl):
     def build(self):
         self.recorder = None
-        self.messages = ft.Column()
+        self.messages = ft.Column(alignment=ft.CrossAxisAlignment.START)
         self.input = ft.TextField(hint_text="Type your message here", expand=True)
         self.record_button = ft.FloatingActionButton(icon=ft.icons.MIC, on_click=self.record_audio)
 
@@ -87,12 +88,16 @@ class ChatterPage(ft.UserControl):
             return ft.Container(
                 content=ft.Text(message),
                 border_radius=10,
+                bgcolor="#0F0F0F",
+                alignment=ft.alignment.top_left,
+
             )
         else:
             return ft.Container(
                         content=ft.Text(message, color="#000000"),
                         border_radius=10,
-                        bgcolor="#F1F0F0",
+                        bgcolor="#F0F0F0",
+                        alignment=ft.alignment.top_left,
                     )
     
         
