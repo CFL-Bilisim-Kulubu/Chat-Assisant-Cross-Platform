d39fb1d378cebd1f5a789abd966ccb66aa91b317
Mert Kalkancı <mertkalkanci05@hotmail.com>
2023-08-06 03:59:13 +0300

    Update audio_manager.py

--- code/audio_manager.py
+++ code/audio_manager.py
@@ -2,56 +2,54 @@ import whisper
 import wave
 import pyaudio
 import threading
+import time
 
 model = whisper.load_model("small.en")
 
-
-try:
-        audio = pyaudio.PyAudio()
-except Exception as e:
-    print(e)
     
 
 def analyze_audio():
     return model.transcribe("audio.wav")["text"]
 
 
-class AudioRecording:
 
+
+class AudioRecording:
     def __init__(self):
-        self.record = False
-        self.frames = []
-        self.stream = None
-        self.audio = None
+        self.record = threading.Event()
+        print(self.record.is_set())
+
 
     def recording(self):
-        if self.record:
+        if self.record.is_set():
             self.stop_recording()
-            self.record = False
+            self.record = threading.Event()
         else:
-            self.record = True
-            self.record_thread =  threading.Thread(target=self.record_audio, args=(self.record))
+            self.record.set()
+            self.record_thread =  threading.Thread(target=self.record_audio, args=())
             self.record_thread.start()
+    
+    def stop_recording(self):
+        self.record = threading.Event()
 
-    def record_audio(record:bool):
+        return "saved successfully"
+    
+
+
+    def record_audio(self):
+        audio = pyaudio.PyAudio()
         frames = []
         stream = audio.open(format=pyaudio.paInt16, channels=1, rate=44100, input=True, frames_per_buffer=1024)
-        while (record):
-            print("recording")
+        while (self.record.is_set()):
             data = stream.read(1024)
             frames.append(data)
         
         stream.stop_stream()
-        audio = wave.open("audio.wav", "wb")
-        audio.setnchannels(1)
-        audio.setsampwidth(audio.get_sample_size(pyaudio.paInt16))
-        audio.setframerate(44100)
-        audio.writeframes(b''.join(frames))
-        audio.close()
+        audio_writer = wave.open("audio.wav", "wb")
+        audio_writer.setnchannels(1)
+        audio_writer.setsampwidth(audio.get_sample_size(pyaudio.paInt16))
+        audio_writer.setframerate(44100)
+        audio_writer.writeframes(b''.join(frames))
+        audio_writer.close()
+        audio.terminate()
         print("recording finished")
-
-    
-    def stop_recording(self):
-        self.record = False
-
-        return "saved successfully"
