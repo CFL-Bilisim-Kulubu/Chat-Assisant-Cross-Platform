6de4224553dd58ad8252f3c17f769e7b80838c64
Mert Kalkancı <mertkalkanci05@hotmail.com>
2023-08-06 14:13:46 +0300

    update llama for llama.c

--- code/llama.py
+++ code/llama.py
@@ -1,23 +1,7 @@
-from transformers import AutoTokenizer
-import transformers
-import torch
+from llama_cpp import Llama
+from file_operations import read_config
 
-model = "meta-llama/Llama-2-7b-hf"
-
-tokenizer = AutoTokenizer.from_pretrained(model)
-pipeline = transformers.pipeline(
-    "text-generation",
-    model=model,
-    torch_dtype=torch.float16,
-    device_map="cpu",
-)
+llm = Llama(model_path="llama.cpp/models/vicuna-7b-1.1.ggmlv3.q4_0.bin")
 
 def generate_response(user_input):
-    return pipeline(
-        user_input,
-        do_sample=True,
-        top_k=10,
-        num_return_sequences=1,
-        eos_token_id=tokenizer.eos_token_id,
-        max_length=200,
-    )[0]
+    return llm(user_input, max_tokens= 400, echo=False)["choices"][0]["text"]
