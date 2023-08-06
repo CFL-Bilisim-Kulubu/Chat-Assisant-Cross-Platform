53124fbe44ec919379d72237208f5dbb28d01ba5
Mert Kalkancı <mertkalkanci05@hotmail.com>
2023-08-06 04:27:00 +0300

    Update llama.py

--- code/llama.py
+++ code/llama.py
@@ -8,7 +8,7 @@ tokenizer = AutoTokenizer.from_pretrained(model)
 pipeline = transformers.pipeline(
     "text-generation",
     model=model,
-    torch_dtype=torch.float32,
+    torch_dtype=torch.float16,
     device_map="cpu",
 )
 
