e9293423b459f2316e7e0fac865e4ccdd6bd0a29
Mert Kalkancı <mertkalkanci05@hotmail.com>
2023-08-07 02:36:33 +0300

    command parsing

--- code/commandparser.py
+++ code/commandparser.py
@@ -17,7 +17,7 @@ class Commands:
         pass
     def reminder(self, time):
         pass
-    def website(self, content):
+    def website(self, website):
         pass
     def meeting(self, time):
         self.reminder(time)
@@ -35,24 +35,51 @@ class CommandParser:
 
         words = command.split(" ").lower()
 
-        for word in words:
+        for index, word in words:
             if word in commandList:
                 self.command = word
                 self.commandFound = True
-                break
+            elif word is "at":
+                try:
+                    self.time = words[index + 1]
+                except:
+                    print("no time")
+                    self.time = None
+
+            elif word is "to":
+                try:
+                    self.to = words[index + 1]
+                except:
+                    print("no to")
+                    self.to = None
+
+            elif word is "content":
+                try:
+                    self.content = words[index + 1]
+                except:
+                    print("no content")
+                    self.content = None
+
+            elif word is "named":
+                try:
+                    self.named = words[index + 1]
+                except:
+                    print("no named")
+                    self.named = None
+
         if self.commandFound:
             if self.command == "email":
-                return Commands.email()
+                return Commands.email(self.to, self.content)
             elif self.command == "text":
-                return Commands.text()
+                return Commands.text(self.to, self.content)
             elif self.command == "reminder":
-                return Commands.reminder()
+                return Commands.reminder(self.time)
             elif self.command == "website":
-                return Commands.website()
+                return Commands.website(self.named)
             elif self.command == "meeting":
-                return Commands.meeting()
+                return Commands.meeting(self.time)
             elif self.command == "book":
-                return Commands.book()
+                return Commands.book(self.named)
         else:
             return None
 
