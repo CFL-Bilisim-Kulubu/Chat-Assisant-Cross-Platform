942a6abe0e6790bc5f53e7449351e392776a6d7a
Mert Kalkancı <mertkalkanci05@hotmail.com>
2023-10-11 01:45:13 +0300

    Update run.bat

--- run.bat
+++ run.bat
@@ -1,9 +1,8 @@
 echo off
+
 call %cd%\venv\Scripts\activate.bat
-echo .
-echo .
-echo By pressing any key you accept the licenses of Meta Llama 2 usage license & ChatGPT API usage API
-echo Llama-2 License: https://ai.meta.com/llama/license/
-echo Terms of Usage OpenAI API https://openai.com/policies/terms-of-use
+
+echo By pressing any key you accept the license of the llama version you have downloaded. Be aware of usage license of it. 
+echo For more detail follow Llama-2 License page: https://ai.meta.com/llama/license/
 pause
 py %cd%\code\main.py
